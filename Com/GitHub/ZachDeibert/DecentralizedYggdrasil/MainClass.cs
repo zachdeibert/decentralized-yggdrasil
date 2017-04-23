5db028e9984c073233a46308fc27e9169a3d1b7d
Zach Deibert <zachdeibert@gmail.com>
2017-04-23 17:00:10 -0500

    Made it launch everything

--- Com/GitHub/ZachDeibert/DecentralizedYggdrasil/MainClass.cs
+++ Com/GitHub/ZachDeibert/DecentralizedYggdrasil/MainClass.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil {
 	class MainClass {
 		public static void Main(string[] args) {
-			YggdrasilServer server = new YggdrasilServer(8080);
+			YggdrasilServer server = new YggdrasilServer(56195);
 			server.Start();
-			Console.WriteLine("Press any key to stop the server");
-			Console.ReadKey(true);
+			Process ssl;
+			if (Type.GetType("Mono.Runtime") == null) {
+				ssl = Process.Start("ssl-endpoint.exe", "*.mojang.com 443 localhost 56195");
+			} else {
+				ssl = Process.Start("mono", "ssl-endpoint.exe *.mojang.com 443 localhost 56195");
+			}
+			Thread.Sleep(1000);
+			X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+			store.Open(OpenFlags.ReadOnly);
+			X509Certificate2 cert = store.Certificates.Cast<X509Certificate2>().First(c => c.Subject == "CN=*.mojang.com");
+			File.WriteAllBytes("mojang.cer", cert.Export(X509ContentType.Cert));
+			Process mc = Process.Start("java", "-jar decentralized-yggdrasil.jar mojang.cer");
+			mc.WaitForExit();
 			server.Stop();
+			ssl.Kill();
 		}
 	}
 }
