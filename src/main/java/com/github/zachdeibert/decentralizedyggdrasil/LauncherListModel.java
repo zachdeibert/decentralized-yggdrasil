3bb4d31e8eecbb2a7dce1561d446f05570d233c5
Zach Deibert <zachdeibert@gmail.com>
2017-04-23 16:30:50 -0500

    Made the launcher list persistant

--- src/main/java/com/github/zachdeibert/decentralizedyggdrasil/LauncherListModel.java
+++ src/main/java/com/github/zachdeibert/decentralizedyggdrasil/LauncherListModel.java
@@ -1,17 +1,34 @@
 package com.github.zachdeibert.decentralizedyggdrasil;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 import javax.swing.AbstractListModel;
 
 public class LauncherListModel extends AbstractListModel<Launcher> {
 	private static final long serialVersionUID = 3708835313787267906L;
 	private final List<Launcher> launchers;
+	private final Preferences prefs;
 
 	public boolean isInstalled(Launcher launcher) {
 		return launchers.contains(launcher) && launcher.isInstalled();
+	}
+
+	private void save() {
+		try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+			try (ObjectOutputStream stream = new ObjectOutputStream(buffer)) {
+				stream.writeObject(launchers);
+			}
+			prefs.putByteArray("ser", buffer.toByteArray());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public boolean install(Launcher launcher) {
@@ -25,6 +42,7 @@ public class LauncherListModel extends AbstractListModel<Launcher> {
 		}
 		launchers.add(launcher);
 		fireIntervalAdded(this, launchers.indexOf(launcher), launchers.indexOf(launcher));
+		save();
 		return true;
 	}
 
@@ -33,6 +51,7 @@ public class LauncherListModel extends AbstractListModel<Launcher> {
 		if (index >= 0) {
 			launchers.remove(index);
 			fireIntervalRemoved(this, index, index);
+			save();
 		}
 	}
 
@@ -48,5 +67,17 @@ public class LauncherListModel extends AbstractListModel<Launcher> {
 
 	public LauncherListModel() {
 		launchers = new ArrayList<Launcher>();
+		prefs = Preferences.userNodeForPackage(getClass());
+		byte[] data = prefs.getByteArray("ser", new byte[0]);
+		if (data.length > 0) {
+			try (ByteArrayInputStream buffer = new ByteArrayInputStream(data);
+					ObjectInputStream stream = new ObjectInputStream(buffer)) {
+				@SuppressWarnings("unchecked")
+				List<Launcher> list = (List<Launcher>) stream.readObject();
+				launchers.addAll(list);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 }
