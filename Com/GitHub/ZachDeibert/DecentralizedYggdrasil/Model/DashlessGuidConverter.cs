967ed3987c937b6df7c375b7e4e63c402b2186e8
Zach Deibert <zachdeibert@gmail.com>
2017-05-17 18:07:22 -0500

    Made join server request serialize correctly

---
+++ Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Model/DashlessGuidConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Model {
+	public class DashlessGuidConverter : JsonConverter {
+		public override bool CanConvert(Type objectType) {
+			return objectType == typeof(Guid);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+			return Guid.Parse(serializer.Deserialize<string>(reader));
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+			serializer.Serialize(writer, new string(value.ToString().Where(c => c != '-').ToArray()));
+		}
+	}
+}
+
--- Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Model/JoinServerRequest.cs
+++ Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Model/JoinServerRequest.cs
@@ -3,9 +3,9 @@ using Newtonsoft.Json;
 
 namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Model {
 	public class JoinServerRequest {
-		[JsonProperty("accessToken")]
+		[JsonProperty("accessToken"), JsonConverter(typeof(DashlessGuidConverter))]
 		public Guid AccessToken;
-		[JsonProperty("selectedProfile")]
+		[JsonProperty("selectedProfile"), JsonConverter(typeof(DashlessGuidConverter))]
 		public Guid ProfileId;
 		[JsonProperty("serverId")]
 		public string ServerHash;
