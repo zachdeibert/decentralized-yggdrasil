7fcc2c45fc9f4083307c22d927896656ccb46d68
Zach Deibert <zachdeibert@gmail.com>
2017-05-17 14:12:13 -0500

    Added proxying to the check join api

--- Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Apis/CheckJoinApi.cs
+++ Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Apis/CheckJoinApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Model;
 
@@ -8,6 +9,7 @@ namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Apis {
 	public class CheckJoinApi : IApi {
 		private List<UserData> Users;
 		private TransientStateData State;
+		private RealYggdrasil Yggdrasil;
 
 		public Type ParamType {
 			get {
@@ -24,6 +26,7 @@ namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Apis {
 		public void Init(YggdrasilServer server, List<UserData> users, TransientStateData state, RealYggdrasil yggdrasil) {
 			Users = users;
 			State = state;
+			Yggdrasil = yggdrasil;
 		}
 
 		public bool IsAcceptable(Uri uri) {
@@ -40,16 +43,20 @@ namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Apis {
 			IEnumerable<Profile> matchedProfiles = Users.SelectMany(u => u.Profiles).Where(p => p.Name == username);
 			foreach (Profile profile in matchedProfiles) {
 				TransientProfileData data = State[profile];
-				UserData user = Users.FirstOrDefault(u => u.Profiles.Any(p => p.Id == profile.Id));
-				if (user == null) {
-					throw new StandardErrorException(StandardErrors.InvalidToken, 403, "Forbidden");
-				} else {
-					RSACryptoServiceProvider rsa = user.RSAPublicKey;
-					if (rsa.VerifyHash(serverHash.SHA1HexToBytes(), CryptoConfig.MapNameToOID("SHA1"), Convert.FromBase64String(data.JoinedServerHash))) {
-						return profile;
+				UserData user = Users.First(u => u.Profiles.Any(p => p.Id == profile.Id));
+				RSACryptoServiceProvider rsa = user.RSAPublicKey;
+				if (rsa.VerifyHash(serverHash.SHA1HexToBytes(), CryptoConfig.MapNameToOID("SHA1"), Convert.FromBase64String(data.JoinedServerHash))) {
+					return profile;
+				} else if (user.TryProxy) {
+					HttpStatusCode status;
+					Profile real = Yggdrasil.Request<Profile>(string.Concat("https://sessionserver.mojang.com/session/minecraft/hasJoined?username=", username, "&serverId=", serverHash), null, out status);
+					if (status == HttpStatusCode.OK) {
+						return real;
 					} else {
 						throw new StandardErrorException(StandardErrors.InvalidToken, 403, "Forbidden");
 					}
+				} else {
+					throw new StandardErrorException(StandardErrors.InvalidToken, 403, "Forbidden");
 				}
 			}
 			throw new StandardErrorException(StandardErrors.InvalidToken, 403, "Forbidden");
