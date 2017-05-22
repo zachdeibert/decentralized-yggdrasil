985b9a7cb97eff95742b8d70e5a53913c2918732
Zach Deibert <zachdeibert@gmail.com>
2017-05-22 07:47:16 -0500

    Added askpass command

---
+++ Com/GitHub/ZachDeibert/DecentralizedYggdrasil/Commands/AskPassCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Com.GitHub.ZachDeibert.DecentralizedYggdrasil.Commands {
+	public class AskPassCommand : ICommand {
+		public string Name {
+			get {
+				return "askpass";
+			}
+		}
+
+		public void Run(string[] args) {
+			Form prompt = new Form() {
+				Width = 500,
+				Height = 150,
+				FormBorderStyle = FormBorderStyle.FixedDialog,
+				Text = "We need to make changes to your computer",
+				StartPosition = FormStartPosition.CenterScreen
+			};
+			Label label = new Label() {
+				Left = 50,
+				Top = 20,
+				Text = "Please enter the password for your account:",
+				Parent = prompt
+			};
+			TextBox input = new TextBox() {
+				Left = 50,
+				Top = 50,
+				Width = 400,
+				PasswordChar = '*',
+				Parent = prompt
+			};
+			Button ok = new Button() {
+				Text = "OK",
+				Left = 350,
+				Width = 100,
+				Top = 70,
+				DialogResult = DialogResult.OK,
+				Parent = prompt
+			};
+			prompt.AcceptButton = ok;
+			ok.Click += (sender, e) => prompt.Close();
+			if (prompt.ShowDialog() == DialogResult.OK) {
+				Console.WriteLine(input.Text);
+			}
+		}
+	}
+}
+
